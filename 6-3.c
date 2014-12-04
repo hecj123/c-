2d051b168e8170668a818251f3e9443b6e9e9098
hecj123 <hecj@simplecloud.cn>
2014-12-04 15:16:39 +0800

    update

---
+++ 6-3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char letter;
+    printf("Enter an upper case letter:");
+    scanf("%c",&letter);
+    
+    if((letter>='A')&&(letter<'Z'))
+    {
+        letter+='a'-'A';   //等同于letter=letter+'a'-'A'; 
+        printf("You entered an uppercase %c.\n",letter);
+    }
+    else
+       printf("You did not enter an uppercase letter.\n");
+       
+    return 0;
