2be6e308c3465addc66798b66ef7655bc0156372
hecj123 <hecj@simplecloud.cn>
2015-01-04 16:04:05 +0800

    update

---
+++ 8-6.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+   char str1[]="People's Republic of";
+   char str2[]="China";
+   printf("%s",strcat(str1,str2));
+   return 0;
+}
---
+++ 8-7.c
@@ -0,0 +1,16 @@
+#include<dtdio.h>
+#include<string.h>
+int main()
+{	char string[30];
+	char ch[3][30];
+	int i;
+	for(i=0;i<3;i++)
+		gets(ch[i]);
+	strcpy(string,ch[0]);
+	for(i=1;i<3;i++)
+		if(strcmp(ch[i],string)<0)
+			strcpy(string,ch[i]);
+	printf("The result is :\n%s\n",string);
+	
+	return 0;
+}
